3733baef99c42f29928cb48668eaba351a7027ce
deprecated@mail.ru <deprecated@mail.ru>
2018-04-23 07:44:25 +0500

    full docker image that works

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -3,3 +3,9 @@
 # save space and clean up apt after install
 echo -e "\n##########\nCleaning up apt after install to reduce image size\n##########\n"
 apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+
+# avvoiding errors during bundle build
+# error, /usr/bin/ld: cannot find -lboost_python3
+# https://github.com/BVLC/caffe/issues/6063
+ln -s /usr/lib/x86_64-linux-gnu/libboost_python-py35.so /usr/lib/x86_64-linux-gnu/libboost_python-py36.so
