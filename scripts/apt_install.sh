a5aa4d07a70f4034b96b49286f46df090724be7b
Столяров Алексей <stolyarov@info74.ru>
2018-04-23 14:45:06 +0500

    test 1.2

--- scripts/apt_install.sh
+++ scripts/apt_install.sh
@@ -4,7 +4,7 @@
 
 echo -e "\n##########\nInstalling all other apt packages\n##########\n"
 apt-get install -yq --no-install-recommends \
-pkg-config zip fakeroot git cmake build-essential nasm wget \
+pkg-config zip fakeroot git cmake build-essential nasm wget curl \
 libusb-1.0-0-dev libglew-dev libglfw3-dev libtbb-dev \
 libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libswscale-dev \
 libavresample-dev ffmpeg libav-tools x264 x265 libportaudio2 portaudio19-dev \
@@ -12,5 +12,5 @@ libboost-dev libboost-python-dev libgoogle-glog-dev libatlas-base-dev libeigen3-
 libceres1 libceres-dev autoconf automake libtool
 
 #install fresh version of pip (pip3)
-wget -sS https://bootstrap.pypa.io/get-pip.py | python3
+curl -sS https://bootstrap.pypa.io/get-pip.py | python3
 
