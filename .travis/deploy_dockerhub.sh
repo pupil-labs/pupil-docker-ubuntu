363085c9e66e55a15b751b312bd9adadbd064d1c
Will Patera <willpatera@gmail.com>
2018-04-26 08:13:33 +0700

    fix repo slug for dockerhub otherwise upload fails

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-TRAVIS_REPO_SLUG=`echo $TRAVIS_REPO_SLUG |  tr '[:upper:]' '[:lower:]'`
+# dockerhub organization name does not match github org name
+# hardcoding the repo slug for debugging
+DOCKERHUB_REPO_SLUG="pupillabs/pupil-docker-ubuntu"
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
@@ -9,6 +11,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t $DOCKERHUB_REPO_SLUG:$TAG .
+docker push $DOCKERHUB_REPO_SLUG
 
