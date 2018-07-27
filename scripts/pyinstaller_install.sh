a38938d4ae26d8ed08d0f95c87fe350f06915cd3
Will Patera <willpatera@gmail.com>
2018-07-27 09:50:14 +0700

    fix - specify python3 for pyinstaller build

--- scripts/pyinstaller_install.sh
+++ scripts/pyinstaller_install.sh
@@ -3,7 +3,7 @@
 cd /root
 git clone --depth=1 https://github.com/pupil-labs/pyinstaller.git
 cd pyinstaller/bootloader
-python ./waf distclean all
+python3 ./waf distclean all
 cd ..
-python setup.py install
+python3 setup.py install
 cd /root && rm -rf pyinstaller
